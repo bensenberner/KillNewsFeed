5215829f70e068a078131843bf85ee7751a74841
Ben Lerner <benlerner95@gmail.com>
2016-02-25 20:54:56 -0500

    modified it to affect pages and groups

--- kill.js
+++ kill.js
@@ -1,5 +1,6 @@
 function blockAndDisplay() {
-    var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
+    //var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
+    var feed = $('#stream_pagelet');
     var message = $('#distracted');
 
     if (feed.length == 0) {
@@ -7,7 +8,7 @@ function blockAndDisplay() {
     } else if (message.length == 0) {
         message = $('<h1>')
             .attr('id', 'distracted')
-            .text("Don't get distracted by Facebook!")
+            .text("Get off of Facebook, Ben.")
             .css('font-size', '30px')
             .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
             .css('position', 'relative')
@@ -15,12 +16,19 @@ function blockAndDisplay() {
         $('[data-location=maincolumn]').append(message);
      }
 
+    // main newsfeed
     feed.children().remove();
-    $('.ticker_stream').remove();
+    $('#tickerActivityStories').remove();
     $('.ego_column').remove();
-    $('#pagelet_games_rhc').remove();
     $('#pagelet_trending_tags_and_topics').remove();
     $('#pagelet_canvas_nav_content').remove();
+
+    // for groups
+    $('#pagelet_group_').remove();
+
+    // for profiles
+    $('#pagelet_main_column_personal').remove()
+    $('div[data-referrer=timeline_light_nav_top]').remove();
 }
 
 window.setInterval(blockAndDisplay, 100);
